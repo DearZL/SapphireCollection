07fcb56559287855e303188af760c452d4d03034
ZL <1427227820@qq.com>
2023-05-25 10:04:55 +0800

    SmallFix

---
+++ enum/commodityStatus.go
@@ -0,0 +1,8 @@
+package enum
+
+const (
+	// CommodityDisabled 已禁止售卖
+	CommodityDisabled = true
+	// CommodityActive 已开启售卖
+	CommodityActive = false
+)
---
+++ enum/userStatus.go
@@ -0,0 +1,8 @@
+package enum
+
+const (
+	// UserDisabled 已禁用
+	UserDisabled = true
+	// UserActive 已启用
+	UserActive = false
+)
